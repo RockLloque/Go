564a73c7cc4c0549f7354176fec2e94f4bf5570d
rocklloque <thmschmahl@gmail.com>
2021-06-12 17:52:47 +0200

    Added errors exercise

---
+++ tour/exercises/errors.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+	//The conversion of e to float64 is neccessary because otherwise Sprintf would
+	//call the surrounding function again, creating an infinite loop.
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z, s := 1.0, 0.0
+	for math.Abs(z-s) >= 1e-15 {
+		s = z
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
